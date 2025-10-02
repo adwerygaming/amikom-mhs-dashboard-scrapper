c0348583b1faf46b5b3a9a2acdb999d6c7c99ca8
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2025-10-02 21:36:29 +0700

    feat(amikom): Add detailed class event logging
    - refactor(amikom): Move TypedEmitter implementation to AmikomService
    - feat(amikom): Log class_finished event details
    - feat(amikom): Log class_started event details
    - feat(amikom): Log class_upcoming_10m event details
    - feat(amikom): Log class_upcoming_15m event details
    - feat(amikom): Log class_upcoming_30m event details
    - feat(amikom): Log class_upcoming_1h event details

--- src/amikom/AmikomService.ts
+++ src/amikom/AmikomService.ts
@@ -8,17 +8,32 @@ import { ConvertDayNameToDate } from "../utils/ConvertDayNameToDate.js";
 import moment from "moment-timezone";
 import { ClassSchedule } from "../types/AmikomTypes.js";
 
-import TypedEmitter from "typed-emitter"
+type AmikomServiceEvents = {
+    class_started: ClassSchedule,
+    class_upcoming_1h: ClassSchedule,
+    class_upcoming_30m: ClassSchedule,
+    class_upcoming_15m: ClassSchedule,
+    class_upcoming_10m: ClassSchedule,
+    class_upcoming_5m: ClassSchedule,
+    class_finished: ClassSchedule,
+    error: any,
+}
 
-type AmikomServiceEvents {
-    class_started: ClassSchedule
-    class_upcoming_1h: ClassSchedule
-    class_upcoming_30m: ClassSchedule
-    class_upcoming_15m: ClassSchedule
-    class_upcoming_10m: ClassSchedule
-    class_upcoming_5m: ClassSchedule
-    class_finished: ClassSchedule
-    error: any
+class TypedEmitter<T> {
+    private ee = new EventEmitter();
+
+    on<K extends keyof T>(event: K, listener: (arg: T[K]) => void): this {
+        this.ee.on(event as string, listener);
+        return this;
+    }
+
+    emit<K extends keyof T>(event: K, arg: T[K]): boolean {
+        return this.ee.emit(event as string, arg);
+    }
+
+    eventNames(): (keyof T)[] {
+        return this.ee.eventNames() as (keyof T)[];
+    }
 }
 
 export class AmikomService extends TypedEmitter<AmikomServiceEvents> {
--- src/amikom/Index.ts
+++ src/amikom/Index.ts
@@ -8,6 +8,32 @@ amikom.on("error", (e: any) => {
     console.error(e)
 })
 
-amikom.on("class_finished", (class) => {
-    
+amikom.on("class_finished", (classData) => {
+    console.log(`[${tags.Amikom}] Class Ended!`)
+    console.log(`[${tags.Amikom}] ${classData.Kelas} - ${classData.NamaDosen}`)
+})
+
+amikom.on("class_started", (classData) => {
+    console.log(`[${tags.Amikom}] Class Started!`)
+    console.log(`[${tags.Amikom}] ${classData.Kelas} - ${classData.NamaDosen}`)
+})
+
+amikom.on("class_upcoming_10m", (classData) => {
+    console.log(`[${tags.Amikom}] Class In 10m!`)
+    console.log(`[${tags.Amikom}] ${classData.Kelas} - ${classData.NamaDosen}`)
+})
+
+amikom.on("class_upcoming_15m", (classData) => {
+    console.log(`[${tags.Amikom}] Class In 15m!`)
+    console.log(`[${tags.Amikom}] ${classData.Kelas} - ${classData.NamaDosen}`)
+})
+
+amikom.on("class_upcoming_30m", (classData) => {
+    console.log(`[${tags.Amikom}] Class In 30m!`)
+    console.log(`[${tags.Amikom}] ${classData.Kelas} - ${classData.NamaDosen}`)
+})
+
+amikom.on("class_upcoming_1h", (classData) => {
+    console.log(`[${tags.Amikom}] Class In 1h!`)
+    console.log(`[${tags.Amikom}] ${classData.Kelas} - ${classData.NamaDosen}`)
 })
